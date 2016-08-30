8af1895b1e4f0dd9bee0a16a31f436cfe53ad317
greenitthe <greenitthe@gmail.com>
2016-08-29 20:05:39 -0500

    Made the output look right. No tests yet!

---
+++ temperature.js
@@ -0,0 +1,15 @@
+var Temperature = {
+  toFahrenheit: function(celsius) {
+    return celsius * (9/5) + 32;
+  },
+  //Don't forget to add that comma above here...
+  toCelsius: function(fahrenheit) {
+    return (fahrenheit - 32) * (5/9);
+  }
+}
+
+var tempToday = 82; //Today's temperature in fahrenheit
+var tempInCelsius = Math.round(Temperature.toCelsius(tempToday));
+var message = 'Today’s temperature is ' + tempToday + '°F, which is ' + tempInCelsius + '°C.';
+
+console.log(message);
