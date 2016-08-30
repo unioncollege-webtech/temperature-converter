38a1c1bf6df1e3abecadbf04b51450b73952c484
greenitthe <greenitthe@gmail.com>
2016-08-29 20:48:22 -0500

    added travis, license

--- test/index.js
+++ test/index.js
@@ -6,11 +6,10 @@ exec('node .', function(error, stdout, stderr) {
     if (error) {
         throw error;
     }
-
     // Try test, give them leeway if they don't include the ° symbol.
     if ((stdout !== "Today's temperature is 82°F, which is 28°C.\n") ||
         (stdout !== "Today's temperature is 82F, which is 28C.\n")) {
-            readFile('./peter-piper.js', "utf-8", function(err, value) {
+            readFile('./temperature.js', "utf-8", function(err, value) {
             if (err) {throw err;}
 
             eval(value);
